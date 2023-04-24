393bac8171f2a65d6296823aa5e13064a2d1b154
Fangwei Cheng <fc4uk@virginia.edu>
2023-04-24 11:57:07 -0400

    fixed a bug in dac..now the time domain reduction should work fine..

--- src/model/resources/dac.jl
+++ src/model/resources/dac.jl
@@ -162,7 +162,7 @@ function dac!(EP::Model, inputs::Dict, setup::Dict)
 
     # get the CO2 balance 
     # the net negative CO2 for each DAC y at each hour t, CO2 emissions from heat consumption minus CO2 captured by DAC = net negative emissions
-    @expression(EP, eCO2_DAC_net[y in DAC_ID, t = 1:T], eDAC_heat_CO2[y,t] - vCO2_DAC[y,t] )
+    @expression(EP, eCO2_DAC_net[y in DAC_ID, t = 1:T], (eDAC_heat_CO2[y,t] - vCO2_DAC[y,t]) )
     # the net negative CO2 from all the DAC
     @expression(EP, eCO2_DAC_net_ByZoneT[z = 1:Z, t = 1:T], 
         sum(eCO2_DAC_net[y, t] for y in dfDac[(dfDac[!, :Zone].==z), :DAC_ID]))
@@ -176,7 +176,7 @@ function dac!(EP::Model, inputs::Dict, setup::Dict)
     # separately account for the amount of CO2 that is captured.
     # actually the eCO2_net should be the total sequestration carbon. since eCO2_DAC_net should be a negative value, put minus sign in front of it..
     # costs associated with co2 transport & storage ($/(t CO2/h)) = captured co2 (t CO2/h) * Co2 transport and storage cost ($/t CO2)
-    @expression(EP, eCCO2_TS_ByPlant[y in DAC_ID, t = 1:T], -eCO2_DAC_net[y, t]* dfDac[y, :CO2_Transport_Storage_Per_t])
+    @expression(EP, eCCO2_TS_ByPlant[y in DAC_ID, t = 1:T], -eCO2_DAC_net[y, t]* dfDac[y, :CO2_Transport_Storage_Per_t]* omega[t])
     # the sequestrated CO2 from all the DAC 
     @expression(EP, eCCO2_TS_ByZoneT[z = 1:Z, t = 1:T], 
         sum(eCCO2_TS_ByPlant[y, t] for y in dfDac[(dfDac[!, :Zone].==z), :DAC_ID]))
